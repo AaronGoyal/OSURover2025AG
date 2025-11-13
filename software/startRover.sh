b1b4fbc28e113539886329e4ce8e9857077bf348
Henry Dalrymple <dalrymhe@oregonstate.edu>
2025-11-12 19:11:30 -0800

    Added odometry to imu, gps, and servo

--- software/startRover.sh
+++ software/startRover.sh
@@ -1,17 +1,18 @@
-sudo modprobe can
-sudo modprobe can_raw
-sudo modprobe mttcan
+#!/bin/bash
+#sudo modprobe can
+#sudo modprobe can_raw
+#sudo modprobe mttcan
 
-sudo ip link set can0 down
-sleep 1
-sudo ip link set can1 down
-sleep 1
-sudo ip link set can1 txqueuelen 150
-sleep 1
-sudo ip link set can1 up type can bitrate 1000000
-sleep 1
-sudo ip link set can0 up type can bitrate 500000
-sleep 1
+#sudo ip link set can0 down
+#sleep 1
+#sudo ip link set can1 down
+#sleep 1
+#sudo ip link set can1 txqueuelen 150
+#sleep 1
+#sudo ip link set can1 up type can bitrate 1000000
+#sleep 1
+#sudo ip link set can0 up type can bitrate 500000
+#sleep 1
 
 source /home/makemorerobot/Rover_2023_2024/software/install/setup.bash
 ros2 launch rover2_main rover2_main_launch.py 2> /home/makemorerobot/Rover_2023_2024/software/output.txt
